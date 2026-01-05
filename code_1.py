28d4c3ce6faef2da62a56974265ae3a4a5dbae9d
akm93245 <149232415+akm93245@users.noreply.github.com>
2026-01-05 19:39:33 +0500

    like we use to do

---
+++ code_1.py
@@ -0,0 +1,17 @@
+'''print("this is all just a foley")
+print("badnam onge to kia naam nahi hoga")
+print("jack sparrow the last pirate quite like the sound of that   ")
+print("you know the problem with being the last of anything")
+print("you get see you own death coming to you and you can't  do anything about it")
+print(69+52)
+print(6*9)
+print(88/8)
+print(99-25)
+print(10%2) '''
+
+#Creating lambda function
+x = 0
+y = 0
+Add  = x + y
+print(Add(2,5))
+
