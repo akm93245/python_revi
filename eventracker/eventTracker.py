a9cfb6ff32005a2587a5fc772eecd1172985c687
akm93245 <149232415+akm93245@users.noreply.github.com>
2026-01-07 17:51:21 +0500

    first comit

---
+++ eventracker/eventTracker.py
@@ -0,0 +1,44 @@
+Events = []
+
+def add_event(name, date, time):
+    event = {
+        'name':name,
+        'date':date,
+        'time':time
+    }
+    
+    Events.append(event)
+
+def add_new_event():
+    name = input("Event Name : ")
+    date = input("Date (dd/mm/yyyy) : ")
+    time = input("Time (hh/mm): ")
+    
+    add_event(name,date,time)
+
+def show_events():
+    for event in Events:
+        print(f"{event['name']:20} {event['date']:20} {event['time']}")    
+
+
+def main():
+    print("\n==============Welcome, To Event Tracker==============")   
+    
+    print("1. Add Event")
+    print("2. Show Events")
+    print("3. Quit")
+    
+    choice = int(input("choice : "))
+    
+    while True:
+        if choice ==1:
+            add_new_event()
+        elif choice == 2:
+            show_events()
+        elif choice ==3:
+            print("Thank you for using Event Tracker You you next time time")
+            break
+        else:
+            print("Invalid choice")
+            
+main()
