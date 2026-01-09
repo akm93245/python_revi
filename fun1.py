fb2190b7a1d958528a2b68829c23d44525c697d3
akm93245 <149232415+akm93245@users.noreply.github.com>
2026-01-09 22:48:41 +0500

    neew

---
+++ fun1.py
@@ -0,0 +1,19 @@
+def show(name , f_name, job,goal):
+    str name
+    str f_name
+    str job
+    str goal
+    
+  '''  input(name)
+    input(f_name)
+    input(job)
+    input(goal)'''
+    
+    print("name : ",name)
+    print("Father name : ",f_name)
+    print("Job title : ",job)
+    print("Life goal : ",goal)
+    
+
+show("Ahmad","M.Ibrahim","Ds","Die")
+
