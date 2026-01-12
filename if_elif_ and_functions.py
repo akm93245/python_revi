1979275a7abdc2485984c9e1b476936a44a0cfcf
akm93245 <149232415+akm93245@users.noreply.github.com>
2026-01-12 14:55:21 +0500

    here

---
+++ if_elif_ and_functions.py
@@ -0,0 +1,29 @@
+# variables 
+
+your_age = int(input("Enter your age :"))
+# your_age=type int (your_age)
+age_req = 19
+# using if and elif statements
+if your_age > 0 and your_age < 7 :
+    print("Play kiddo and Enjoy the days of inocense")
+
+elif your_age > 7 and your_age < 16 :
+    print("Go to school kiddo ")
+
+elif your_age > 16 and your_age < 19 :
+    print("college time boy ")
+
+
+elif your_age >= 19 and your_age <= 30 :
+    print("my boy you are welcome to join")
+
+elif your_age > 30 and your_age < 45 :
+    print("create some kids")
+
+elif your_age > 45 and your_age < 70 :
+    print("play with grand kids")
+
+elif your_age >= 70 :
+    print("Just Die")
+    
+else : print("What ?????")
