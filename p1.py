0f0759233665dfc7bb9a9868e82a6a8f1845b604
akm93245 <149232415+akm93245@users.noreply.github.com>
2026-01-15 20:20:46 +0500

    ok hai

---
+++ p1.py
@@ -0,0 +1,13 @@
+#This code is revising purpose
+
+num = int(input("Enter the age : "))
+
+if num > 18:
+    print(" we are selecting happy ones you are and adult. Adults doesn't have happiness ")
+    print("your shit life ")
+    
+elif num<=10 and num >18 :
+    print("focus on your heart not on the world. Be HAPPY   -_-  ")
+else:
+    print("you can be happy for no reason enjoy the days of innocence")\
+    
