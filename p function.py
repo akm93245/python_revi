08c2a9e167ef76840d539dffad0e4eae364e804c
akm93245 <149232415+akm93245@users.noreply.github.com>
2026-01-15 20:07:35 +0500

    okk

---
+++ p function.py
@@ -0,0 +1,16 @@
+#using function in python
+
+def student(name , roll ,dept):
+    print("               ")
+    print("Student Details |_| ")
+    print("Name : ",name)
+    print("Roll no : ", roll)
+    print("Department : ", dept)
+    print("__________________________________________________________")
+   
+    
+
+student("Ahmad",11,"Data Science")
+student("Waqar ",10,"Data Science")
+student("Saleem",31,"Computer Science")
+student("Shahsawar",29,"English")
